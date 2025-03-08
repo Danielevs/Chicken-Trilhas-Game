108a667cac49eef4642eac20df553e4d8df67955
Daniele <danielevs012@gmail.com>
2025-03-08 11:24:56 -0400

    First Commit

---
+++ Assets/Player/PlayerContactHandler.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+public class PlayerContactHandler : MonoBehaviour
+{
+    public Image itemImage;
+    public PlayerAudioController audioController;
+     public string nextLevelName = "Level 1";
+    bool canWinLevel = false;
+
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Enemy"))
+        {
+             SceneManager.LoadScene("GameOver");
+        }
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+         if (collision.gameObject.CompareTag("Item"))
+        {
+
+            Destroy(collision.gameObject);
+            itemImage.color = Color.white;
+            canWinLevel = true;
+            audioController.PlayGetItem();
+
+        }
+
+         if (collision.gameObject.CompareTag("FinalPoint"))
+        {
+            if (canWinLevel)
+            {
+                SceneManager.LoadScene(nextLevelName);
+            } else
+            {
+                Debug.Log("Ainda não venceu");
+            }
+        }
+    }
+}
